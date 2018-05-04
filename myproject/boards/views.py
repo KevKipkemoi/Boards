6ec3162f0033a549e610b9113c677da1e2da9a34
Kev Kipkemoi <kevkipkemoi@outlook.com>
2018-05-04 07:54:47 +0300

    Refactoring views 404

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -7,8 +7,5 @@ def home(request):
     return render(request, 'home.html', {'boards': boards})
 
 def board_topics(request, pk):
-    try:
-        board = Board.objects.get(pk=pk)
-    except Board.DoesNotExist:
-        raise Http404
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
